a699e37b28cc2488c7e400c0c6018bb543682ebe
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-11 19:49:14 -0300

    Capitulo 1 Parte 4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- // swcMinify: true,
+  // swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
