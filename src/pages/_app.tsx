abae750dc348fdd109b482d995cc084ee06e5eeb
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-11 19:24:48 -0300

    Capitulo 1 Parte 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
+import { globalStyes } from '../styles/global'
 
+globalStyes()
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
