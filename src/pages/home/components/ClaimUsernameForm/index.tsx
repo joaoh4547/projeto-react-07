09158c4ae0f30bf79d5f008afa44c905f8dd3215
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-13 19:22:20 -0300

    Capitulo 2 PArte 3

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,33 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const claimUserNameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormSchema = z.infer<typeof claimUserNameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormSchema>({
+    resolver: zodResolver(claimUserNameFormSchema),
+  })
+
+  function handleClaimUser(data: unknown) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="call.io/" placeholder="Seu Usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUser)}>
+      <TextInput
+        size="sm"
+        prefix="call.io/"
+        placeholder="Seu Usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar <ArrowRight />
       </Button>
