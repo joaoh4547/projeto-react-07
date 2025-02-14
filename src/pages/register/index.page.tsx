50bae5653205c2835d9f6bbe275df5110101d22a
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-13 21:44:06 -0300

    Capitulo 2 Parte 5

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,31 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Form, Header } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="call.io/" placeholder="Seu Usuario" />
+        </label>
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu Nome" />
+        </label>
+        <Button type="submit">
+          Proximo passo <ArrowRight weight="bold" />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
