5055f2b06d5e557b10fed114bdfe4a385799ed1f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-15 13:32:16 -0300

    Capitulo 3 Parte 1

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,33 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight } from 'phosphor-react'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  // async function handleRegisterUser(data: RegisterUserSchema) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Agenda</Text>
+          <Button variant="secondary" size="sm">
+            Conectar <ArrowRight weight="bold" />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Proximo passo <ArrowRight weight="bold" />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -48,6 +48,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (isAxiosError(error)) {
         if (error?.response?.data?.message) {
