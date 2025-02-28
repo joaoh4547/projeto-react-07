557673f7db049a1b0b357cfdbe00a44626a25c40
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-28 19:42:03 -0300

    Capitulo  6 Parte 2

---
+++ src/components/index.tsx
@@ -0,0 +1,56 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDays } from '../utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Fevereiro <span>2025</span>
+        </CalendarTitle>
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((day, index) => (
+              <th key={index}>{day}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <CalendarDay>1</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>2</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>3</CalendarDay>
+          </td>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '../../../../../components'
+import { Container } from './styles'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar></Calendar>
+    </Container>
+  )
+}
