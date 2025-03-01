6e0361e09d73e850c0572b3809f6720b20b2a7a3
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-01 17:54:07 -0300

    Capitulo 6 Parte 5

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,5 +1,40 @@
-import { CalendarStep } from '../CalendarStep'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
 
-export function ScheduleForm() {
-  return <CalendarStep />
+export function ConfirmStep() {
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          01 de fevereiro de 2025
+        </Text>
+        <Text>
+          <Clock />
+          17:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size="sm">Nome Completo</Text>
+        <TextInput placeholder="Seu Nome" />
+      </label>
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="email@example.com" />
+      </label>
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+      <FormActions>
+        <Button variant="tertiary" type="button">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
