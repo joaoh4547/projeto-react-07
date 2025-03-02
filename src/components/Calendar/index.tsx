f30337fcb0f753ddbd2066f4f15c8da61829b0cb
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-02 16:11:15 -0300

    Capitulo 6 Parte 16

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -10,6 +10,9 @@ import {
 import { getWeekDays } from '../../utils/get-week-days'
 import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '../../lib/axios'
+import { useRouter } from 'next/router'
 
 interface CalendarWeek {
   week: number
@@ -17,6 +20,10 @@ interface CalendarWeek {
     date: dayjs.Dayjs
     disabled: boolean
   }>
+}
+
+interface BlockedDates {
+  blockedWeekDays: number[]
 }
 
 type CalendarWeeks = CalendarWeek[]
@@ -30,6 +37,10 @@ export function Calendar({ onDateSelect, selectedDate }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
+
+  const { query } = useRouter()
+
+  const username = String(query.username)
 
   function handlePreviousMonth() {
     const previousMonthDate = currentDate.subtract(1, 'month')
@@ -46,6 +57,28 @@ export function Calendar({ onDateSelect, selectedDate }: CalendarProps) {
   const currentMonth = currentDate.format('MMMM')
 
   const currentYear = currentDate.format('YYYY')
+
+  const { data: blockedDates } = useQuery({
+    queryFn: async () => {
+      const response = await api.get<BlockedDates>(
+        `/users/${username}/blocked-dates`,
+        {
+          params: {
+            year: currentDate.get('year'),
+            month: currentDate.get('month'),
+          },
+        },
+      )
+
+      return response.data
+    },
+    queryKey: [
+      'blocked-dates',
+      username,
+      currentDate.get('year'),
+      currentDate.get('month'),
+    ],
+  })
 
   const calendarWeeks = useMemo(() => {
     const daysInMonthArray = Array.from({
@@ -74,7 +107,12 @@ export function Calendar({ onDateSelect, selectedDate }: CalendarProps) {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: date.endOf('day').isBefore(new Date()) }
+        return {
+          date,
+          disabled:
+            date.endOf('day').isBefore(new Date()) ||
+            blockedDates?.blockedWeekDays.includes(date.get('day')),
+        }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -92,7 +130,7 @@ export function Calendar({ onDateSelect, selectedDate }: CalendarProps) {
     )
 
     return calendarWeeks
-  }, [currentDate])
+  }, [currentDate, blockedDates])
 
   return (
     <CalendarContainer>
---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { year, month } = req.query
+
+  if (!year || !month) {
+    return res
+      .status(400)
+      .json({ message: "Missing 'date' or 'month' parameter" })
+  }
+
+  const user = await prisma.user.findUnique({ where: { username } })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    where: { user_id: user.id },
+    select: { week_day: true },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter(
+    (day) => !availableWeekDays.some((a) => a.week_day === day),
+  )
+
+  return res.json({ blockedWeekDays })
+}
